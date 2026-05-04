920fc38051655c7251703c7226e3834db6fa62c4
Alejandro <alejandroogutierrez06@gmail.com>
2026-05-04 15:44:01 +0200

    Project Setup

---
+++ src/main/java/org/example/flash_sales/FlashSalesApplication.java
@@ -0,0 +1,13 @@
+package org.example.flash_sales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlashSalesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlashSalesApplication.class, args);
+    }
+
+}
