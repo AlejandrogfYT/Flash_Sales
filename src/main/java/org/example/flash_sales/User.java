dce6a190de6b1822d5e0d944b7d235208f76cb9a
Alejandro <alejandroogutierrez06@gmail.com>
2026-05-13 17:32:43 +0200

    Create User Entity

---
+++ src/main/java/org/example/flash_sales/User.java
@@ -0,0 +1,67 @@
+package org.example.flash_sales;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    private String username;
+
+    @NotNull
+    private String password;
+
+    @Email
+    @NotNull
+    private String email;
+
+    @Enumerated(EnumType.STRING)
+    private UserType type;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public UserType getType() {
+        return type;
+    }
+
+    public void setType(UserType type) {
+        this.type = type;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
---
+++ src/main/java/org/example/flash_sales/UserType.java
@@ -0,0 +1,7 @@
+package org.example.flash_sales;
+
+public enum UserType {
+    PARTNER,
+    PUBLIC,
+    INTERNAL
+}
