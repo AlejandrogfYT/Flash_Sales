b640f91c4d4dda7b95bd4d966d4ac39ec798f079
Alejandro <alejandroogutierrez06@gmail.com>
2026-05-18 16:16:09 +0200

    Complete Product Class

--- src/main/java/org/example/flash_sales/Product.java
+++ src/main/java/org/example/flash_sales/Product.java
@@ -2,7 +2,23 @@ package org.example.flash_sales;
 
 
 import jakarta.persistence.Entity;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 
 @Entity
 public class Product extends SellableItem {
+
+    @NotNull
+    @Min(0)
+    @Max(1)
+    private float offer;
+
+    public float getOffer() {
+        return offer;
+    }
+
+    public void setOffer(float offer) {
+        this.offer = offer;
+    }
 }
